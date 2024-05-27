7056ad8247f131e9bea88f49f6c0d9d04b18c48c
Raul Castro <castro.lab@gmail.com>
2024-05-26 20:32:18 -0500

    feat: Add default admin user through migration
    - Created migration to add a default admin user to the users table
    - Added logic to insert the admin user with a hashed password and remember token

---
+++ database/migrations/2024_05_27_012836_add_default_user_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AddDefaultUserToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('GeoSnapPass'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->where('email', 'admin@example.com')->delete();
+    }
+}
